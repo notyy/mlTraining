7245646131142ac198fec82ed89921570bad6e75
notyy <notyycn@gmail.com>
2015-07-01 17:17:20 +0800

    a decision tree sample that merely works

--- src/main/scala/ml/Titanic.scala
+++ src/main/scala/ml/Titanic.scala
@@ -1,11 +1,15 @@
 package ml
 
+import com.typesafe.scalalogging.slf4j.StrictLogging
 import org.apache.spark.SparkContext
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.tree.model.DecisionTreeModel
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{SQLContext, DataFrame}
+import org.apache.spark.sql.{Row, SQLContext, DataFrame}
 import org.apache.spark.sql.functions._
 
-object Titanic {
+object Titanic extends StrictLogging {
   type TrainingSetPath = String
   type Discrete = Int
   type Linear = Double
@@ -20,10 +24,47 @@ object Titanic {
     import sqlContext.implicits._
 
     val data = sc.textFile(path)
+    data.countByValue()
     data.map(_.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1)).map(s =>
       Survivor(s(0), s(1).toInt, s(2).toInt, s(3), s(4), if (s(5).isEmpty) -1 else s(5).toDouble,
         s(6).toInt, s(7).toInt, s(8), s(9).toDouble, s(10), s(11))).toDF()
 //    (?=([^\"]*\"[^\"]*\")*[^\"]*$)
+  }
+
+  import org.apache.spark.mllib.linalg.{Vector, Vectors}
+  def dataFrame2LabeledPoints: DataFrame => RDD[LabeledPoint] = df => {
+    df.map{
+      case Row(id,survived:Int,pClass:Int,name: String,sex: String,age: Double,
+      sibSp:Int,parch:Int,tickNo: String,fare:Double,cabin:String,emarkPort:String) =>
+        LabeledPoint(survived,
+          Vectors.dense(pClass.toDouble,if(sex == "female") 0.0 else 1.0, age,sibSp.toDouble,parch.toDouble,fare))
+    }
+  }
+
+  def train: RDD[LabeledPoint] => DecisionTreeModel = data => {
+    // Split the data into training and test sets (30% held out for testing)
+    val splits = data.randomSplit(Array(0.7, 0.3))
+    val (trainingData, testData) = (splits(0), splits(1))
+
+    // Train a DecisionTree model.
+    //  Empty categoricalFeaturesInfo indicates all features are continuous.
+    val numClasses = 2
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val impurity = "gini"
+    val maxDepth = 1
+    val maxBins = 32
+
+    val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+      impurity, maxDepth, maxBins)
+
+    // Evaluate model on test instances and compute test error
+    val labelAndPreds = testData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+    val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
+    println("Test Error = " + testErr)
+    model
   }
 
   /**
--- src/test/scala/ml/TitanicSpec.scala
+++ src/test/scala/ml/TitanicSpec.scala
@@ -1,10 +1,12 @@
 package ml
 
+import com.typesafe.scalalogging.slf4j.StrictLogging
+import org.apache.spark.sql.SQLContext
 import org.scalatest.{FunSpec, ShouldMatchers}
 import sample.SparkContextHelper
 import Titanic._
 
-class TitanicSpec extends FunSpec with ShouldMatchers with SparkContextHelper {
+class TitanicSpec extends FunSpec with ShouldMatchers with SparkContextHelper with StrictLogging {
   describe("Titanic module") {
     it("contains function that can detect na values from rdd") {
       withLocalSparkContext("titanic") { sc =>
@@ -16,6 +18,25 @@ class TitanicSpec extends FunSpec with ShouldMatchers with SparkContextHelper {
       withLocalSparkContext("titanic") { sc =>
         val rdd = sc.textFile("data/train.csv")
 //        checkNA(rdd.map(_.split(",", -1))).toSet shouldBe Set(2, 1)
+      }
+    }
+
+    it("can turn data into labeled points"){
+      withLocalSparkContext("titanic") { sc =>
+        val sqlContext = new SQLContext(sc)
+        val df = sqlContext.read.parquet("data_parquet")
+        val labeledPoints = dataFrame2LabeledPoints(df)
+        labeledPoints.count() shouldBe df.count()
+      }
+    }
+
+    it("can calculate prediction model from labeled points"){
+      withLocalSparkContext("titanic") { sc =>
+        val sqlContext = new SQLContext(sc)
+        val df = sqlContext.read.parquet("data_parquet")
+        val labeledPoints = dataFrame2LabeledPoints(df)
+        val model = train(labeledPoints)
+        println(model.toDebugString)
       }
     }
   }
